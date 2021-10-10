61332a2fad901cbf9131fe4084798d3e379530c3
Leci1259 <lreid31@gatech.edu>
2021-10-10 14:22:40 -0400

    didn't realize there was a develop folder to guide code in; put all files I did in an old folder; constructed generate markdown.js

--- index.js
+++ index.js
@@ -1,130 +1,13 @@
-//grab packages needed for application
-const fs =require('fs');
-const inquirer = require('inquirer');
+// TODO: Include packages needed for this application
 
-//use inquirer
-inquirer
-    //ask questions
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is your project title?',
-            name: 'title',
-        },
-        {
-            type: 'input',
-            message: 'Give a description of your project',
-            name: 'description',
-        },
-        {
-            type: 'input',
-            message: 'Type out install code',
-            name: 'installCode',
-        },
-        {
-            type: 'input',
-            message: 'How are you using this?',
-            name: 'usage',
-        },
-        {
-            type: 'input',
-            message: 'Who are your contributers?',
-            name: 'contribution',
-        },
-        {
-            type: 'input',
-            message: 'Test Code?',
-            name: 'tests',
-        },
-        {
-            type: 'rawlist',
-            message: 'What kind of license?',
-            name: 'license',
-            choices:["MIT",  "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
-        },
-        {
-            type: 'input',
-            message: 'Github username?',
-            name: 'github',
-        },
-        {
-            type: 'input',
-            message: 'Email?',
-            name: 'email',
-        },
-        {
-            type: 'input',
-            message: 'How should I reach you?',
-            name: 'contact',
-        },
-    ])
-    //use answers
-    .then(function(answers) {
-        var licenseTxt;
-        var licenseBadge;
+// TODO: Create an array of questions for user input
+const questions = [];
 
-        //check for license type
-        switch (answers.license) {
-            case 'MIT':
-                licenseTxt="Released under MIT License Copyright (c) 2013 Mark Otto. Copyright (c) 2017 Andrew Fong";
-                licenseBadge="[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-                break;
-            case 'APACHE 2.0':
-                licenseTxt="Apache License Version 2.0, January 2004 "
-                licenseBadge="[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
-                break;
-            case 'GPL 3.0':
-                licenseTxt="Copyright © 2007 Free Software Foundation, Inc.";
-                licenseBadge="[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-                break;
-            case 'BSD 3':
-                licenseTxt= "BSD-3-Clause Copyright (c) . All rights reserved.";
-                licenseBadge="[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
-                break;
-            case 'None':
-                licenseTxt= 'None';
-        };
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
 
-        //fill string template literal with answers
-        const txt = `# ${answers.title} \n
-        ${licenseBadge} \n
-        ## Description \n
-        ${answers.description} \n
-        
-        ## Table of Contents \n
-        - [Description](#description) \n
-        - [Installation](#installation) \n
-        - [Usage](#usage) \n
-        - [Contribution](#credits) \n
-        - [Tests](#tests) \n
-        - [Questions](#questions) \n
-        - [License](#license) \n
-        
-        ## Installation \n
-        Install Node.js. Run the following command in the terminal:
-        
-        ${answers.installCode} \n
-        ## Usage \n
-        ${answers.usage} \n
-        
-        ## Contribution \n
-        ${answers.contribution} \n
-        
-        ## Tests \n
-        ${answers.tests} \n
-        
-        ## Questions \n
-        ${answers.contact} \n
-        <a href="mailto:${answers.email}">Send me an email if you have any questions.</a> \n
-        <a href="https://github.com/${answers.github}">Github for ${answers.github}</a> \n
-        
-        
-        ## License \n
-        ${licenseTxt} \n`;
+// TODO: Create a function to initialize app
+function init() {}
 
-
-        //output to new file
-        fs.writeFile('generatedREADME.md', txt, (err) =>
-            err ? console.error(err) : console.log('Success!'))
-        ;
-    });
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,136 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
+      break;
+    case "APACHE 2.0":
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]";
+      break;
+    case "GPL 3.0":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
+      break;
+    case "BSD 3":
+      return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]";
+      break;
+    case "None":
+      return "";
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return "(https://opensource.org/licenses/MIT)";
+      break;
+    case "APACHE 2.0":
+      return "(https://opensource.org/licenses/Apache-2.0)";
+      break;
+    case "GPL 3.0":
+      return "(https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "BSD 3":
+      return "(https://opensource.org/licenses/BSD-3-Clause)";
+      break;
+    case "None":
+      return "";
+      break;
+  }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  switch (license) {
+    case "MIT":
+      return `
+## License
+Released under MIT License Copyright (c) 2013 Mark Otto. Copyright (c) 2017 Andrew Fong 
+[${license}]${renderLicenseLink(license)}`;
+      break;
+    case "APACHE 2.0":
+      return `
+## License
+Apache License Version 2.0, January 2004 
+[${license}]${renderLicenseLink(license)}`;
+      break;
+    case "GPL 3.0":
+      return `
+## License
+Copyright © 2007 Free Software Foundation, Inc.
+[${license}]${renderLicenseLink(license)}`;
+      break;
+    case "BSD 3":
+      return `
+## License
+BSD-3-Clause Copyright (c) . All rights
+ [${license}]${renderLicenseLink(license)}`;
+      break;
+    case "None":
+      return "";
+      break;
+  }
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(answers) {
+  return `# ${answers.title} 
+  
+  ${renderLicenseBadge(answers.license)}${renderLicenseLink(answers.license)} 
+ 
+  ## Description 
+
+  ${answers.description} 
+  
+  ## Table of Contents
+
+  - [Description](#description) 
+
+  - [Installation](#installation)
+  
+  - [Usage](#usage) 
+
+  - [Contribution](#credits) 
+
+  - [Tests](#tests) 
+
+  - [Questions](#questions) 
+
+  - [License](#license) 
+
+  
+  ## Installation
+
+  Install Node.js. Run the following command in the terminal:
+  
+  ${answers.installCode}
+
+  ## Usage
+
+  ${answers.usage}
+  
+  ## Contribution 
+
+  ${answers.contribution}
+  
+  ## Tests 
+
+  ${answers.tests}
+  
+  ## Questions
+
+  ${answers.contact}
+  Email:  ${answers.email}
+  Github: [Github Repo](https://github.com/${answers.github})
+  
+  
+  ## License
+
+  ${licenseTxt}`
+;
+}
+
+module.exports = generateMarkdown;
